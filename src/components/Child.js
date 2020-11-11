e61727e8d3f0b06f765da808d17ff24e6b430ad0
Nabendu <nabendu.biswas@gmail.com>
2020-11-11 07:19:02 +0530

    Part-6 complete

---
+++ src/components/Child.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { UserContext, ChannelContext } from '../App'
+
+const Child = () => {
+    return (
+        <div>
+            <UserContext.Consumer>
+                { user => {
+                    return (
+                        <ChannelContext.Consumer>
+                            {channel => {
+                                return (
+                                    <>
+                                        <h1>Child Component with context value - {user}</h1>
+                                        <h2>Channel is - {channel}</h2>
+                                    </>
+                                )
+                            }}
+                        </ChannelContext.Consumer>
+                    )
+                }}
+            </UserContext.Consumer>
+            
+        </div>
+    )
+}
+
+export default Child
